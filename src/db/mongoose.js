30b9c5eb4b4520b57723a33ec8aa67fcb51d16a3
Sharad kumar <ks7053898905@gmail.com>
2020-04-12 06:27:25 -0700

    few changes

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,3 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb+srv://portfolio-database:Sharad@185@cluster0-io0e3.mongodb.net/Portfolio-api?retryWrites=true&w=majority',{useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false})
+mongoose.connect('mongodb+srv://portfolio-database:Sharad123@cluster0-io0e3.mongodb.net/Portfolio-api?retryWrites=true&w=majority',{useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false})
  
